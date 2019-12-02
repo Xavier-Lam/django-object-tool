ccab76baa23615e2970563797325d4aa42354ea0
xavier-lam <13599838712@hotmail.com>
2019-12-03 00:52:19 +0800

    fix import error in new version of django, automatic unittest

--- object_tool/template/loaders.py
+++ object_tool/template/loaders.py
@@ -4,11 +4,14 @@ from __future__ import unicode_literals
 import os
 
 from django.template.loaders.filesystem import Loader as FilesystemLoader
-from django.utils import lru_cache
+try:
+    from functools import lru_cache
+except ImportError:
+    from django.utils.lru_cache import lru_cache
 
 
 class Loader(FilesystemLoader):
-    @lru_cache.lru_cache(maxsize=None)
+    @lru_cache(maxsize=None)
     def get_dirs(self):
         from .. import BASE_DIR
         template_dir = os.path.join(BASE_DIR, "templates")
